7f9d5db386fe3a96b4f9e1de79949f80dd15e2ca
pekochan069 <pekochan069@gmail.com>
2026-07-23 13:45:14 +0900

    feat: add complexity measure lint plugin

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from "oxlint";
 
 export default defineConfig({
   plugins: ["eslint", "jsdoc", "node", "oxc", "promise", "typescript", "unicorn"],
+  jsPlugins: ["oxlint-plugin-complexity"],
   rules: {
     "typescript/no-deprecated": "error",
+    "complexity/complexity": [
+      "error",
+      {
+        cyclomatic: 20,
+        cognitive: 15,
+      },
+    ],
   },
   categories: {
     correctness: "error",
